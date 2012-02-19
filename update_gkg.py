291ea37a3549a8b8730918a748b04a02c3d066d5
Tim 'mithro' Ansell <mithro@mithis.com>
2012-02-19 17:50:06 +1100

    GKG updater also removes obsolete keys now.

--- update_gkg.py
+++ update_gkg.py
@@ -6,11 +6,20 @@ Script to update the DS keys on dlv.isc.org "look asside" DNSSEC system.
 """
 
 import ConfigParser
+import color
 import logging
+import pprint
 import os
 import simplejson
 import sys
 import urllib2
+
+class DeleteRequest(urllib2.Request):
+  def __init__(self, *args, **kwargs):
+    self._method = kwargs.pop('method', 'DELETE')
+    urllib2.Request.__init__(self, *args, **kwargs)
+  def get_method(self):
+    return self._method
 
 # Check the input arguments
 domain = sys.argv[1]
@@ -25,22 +34,6 @@ config.readfp(open(configfile))
 username = config.get('gkg', 'username')
 password = config.get('gkg', 'password')
 
-#"https://www.gkg.net/ws/domain/%s/ds" % domain
-
-# "Save" on the Manage DS Records window
-dsrecords = []
-for line in file(dsset):
-  # thousandparsec.com.     IN DS 57841 5 2 DEA43569E59B6F2FEB99B799BCD39927769A3CA369A846122DE11595 D31A728A
-  ds_domain, in_, ds_, keytag, algorithm, digest_type, digest = line.split(None, 6)
-
-  dsrecords.append({
-      'digest': "".join(digest.split()),
-      'digestType': digest_type,
-      'algorithm': algorithm,
-      'keyTag': keytag,
-      'maxSigLife': 3600*24*90, # FIXME(tansell): Defaulting to 90 days...
-      })
-
 mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
 mgr.add_password(
     None,
@@ -51,13 +44,41 @@ auth_handler = urllib2.HTTPBasicAuthHandler(mgr)
 opener = urllib2.build_opener(auth_handler)
 urllib2.install_opener(opener)
 
-for dsrecord in dsrecords:
+url = "https://www.gkg.net/ws/domain/%s/ds" % domain
+current_records = simplejson.loads(urllib2.urlopen(url).read())
+
+# "Save" on the Manage DS Records window
+new_records = []
+for line in file(dsset):
+  # thousandparsec.com.     IN DS 57841 5 2 DEA43569E59B6F2FEB99B799BCD39927769A3CA369A846122DE11595 D31A728A
+  ds_domain, in_, ds_, keytag, algorithm, digest_type, digest = line.split(None, 6)
+
+  new_records.append({
+      'digest': "".join(digest.split()),
+      'digestType': digest_type,
+      'algorithm': algorithm,
+      'keyTag': keytag,
+      'maxSigLife': str(3600*24*90), # FIXME(tansell): Defaulting to 90 days...
+      })
+
+to_add = []
+for dsrecord in sorted(current_records):
+  dsid = dsrecord['keyTag']+" "+dsrecord['algorithm']
+  if dsrecord in new_records:
+    print color.green("Current %s" % dsid)
+  else:
+    print color.red("Removing %s" % dsid)
+    req = DeleteRequest(
+        url="https://www.gkg.net/ws/domain/%s/ds/%s" % (domain, dsrecord['digest']),
+        )
+    print urllib2.urlopen(req).read()
+
+for dsrecord in sorted(new_records):
+  if dsrecord in current_records:
+    continue
+
+  dsid = dsrecord['keyTag']+" "+dsrecord['algorithm']
+  print color.yellow("Adding %s" % dsid)
   url = "https://www.gkg.net/ws/domain/%s/ds" % domain
-  try:
-    r = urllib2.urlopen(url, data=simplejson.dumps(dsrecord))
-    print r.read()
-  except urllib2.HTTPError, e:
-    if e.code == 403:
-      print "DS record already existed."
-    else:
-      raise
+  r = urllib2.urlopen(url, data=simplejson.dumps(dsrecord))
+  print r.read()
